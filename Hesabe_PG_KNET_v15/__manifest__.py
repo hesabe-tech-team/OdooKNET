b8d9397abc76aa23385912f8b8e9b3db3e1eae72
jimmywebexpert <jshah@hesabe.com>
2023-01-15 13:17:25 +0300

    Odoo KNET Version 15.0.0.0

---
+++ Hesabe_PG_KNET_v15/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hesabe PG For KNET",
+    'author': "Hesabe",
+    'website': "https://developer.hesabe.com/docs/2.0/kits/odoo",
+    'version': '15.0.0.0',
+    'sequence': -98,
+    'maintainer' : 'Jamal shah',
+    'category': 'Accounting/Payment Acquirers',
+    'summary': 'Payment Acquirer: Hesabe Implementation',
+    'description': """Hesabe Payment Gateway For KNET""",
+    'company': 'Hesabe Company for Electronic Payments & Settlements',
+    'depends': ['payment'],
+    'data': [
+        'views/payment_hesabe_templates.xml',
+        'views/payment_views.xml',
+        'data/payment_acquirer_data.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'uninstall_hook': 'uninstall_hook',
+    'license': 'LGPL-3',
+}
---
+++ Hesabe_PG_KNET_v15/controllers/controllers.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import json
+import pprint
+import werkzeug
+
+from odoo import http
+from odoo.http import request
+import PyPDF2
+from odoo.addons.Hesabe_PG_KNET_v15.models.hesabecrypt import decrypt
+
+class HesabeController(http.Controller):
+     
+    @http.route(['/payment/hesabe/knet/return',
+                '/payment/hesabe/knet/fail'], type='http', auth='public', csrf=False, methods=['GET'], save_session=False 
+    )
+    def hesabe_knet_return(self, **post):
+        hesabe = request.env['payment.acquirer'].search([('provider', '=', 'Hesabe_PG_KNET_v15')], limit=1).sudo()
+        data = decrypt(post['data'], hesabe.secret_key, hesabe.iv_key)
+        response = json.loads(data)
+        if post:
+            gres=request.env['payment.transaction'].sudo()._handle_feedback_data('Hesabe_PG_KNET_v15', response)
+        return werkzeug.utils.redirect('/payment/status')
+        
+    return_data_url = '/payment/hesabe'
+    @http.route(
+        return_data_url, type='http', auth="public", csrf=False, methods=['POST'], save_session=False )
+    
+    def hesabe_payment(self, **post):
+        return werkzeug.utils.redirect(post.get('form_url'))
